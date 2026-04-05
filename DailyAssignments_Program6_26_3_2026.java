e9bb9701508d5654b12a9889400e4a548bffac5f
jyoti <34053428+24EC@users.noreply.github.com>
2026-04-05 09:08:55 +0530

    Add files via upload

--- DailyAssignments_Program6_26_3_2026.java
+++ DailyAssignments_Program6_26_3_2026.java
@@ -4,54 +4,27 @@ public class DailyAssignments_Program6_26_3_2026 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//A number that equals the sum of its own digits, each raised to the power of the total number of digit
+   
 		
-		int subject1 = 38;
-		int subject2 = 39;
-		int subject3 = 43;
-		int subject4 = 34;
-		int subject5 = 35;
+		int num =124;
+		int originalnum=num;
+		int cubesum=0;
 		
 		
-	   //Marks of 5 subjects (out of 100 each) Pass/fail
-
-		if(subject1<33 || subject2<33 || subject3<33 || subject4<33 || subject5<33) 
-			System.out.println("Fail");
-		else 
-			System.out.println("Pass");
-		
-		//Calculate percentage and print percentage
+		while(num>0) {
+			int lastDigit = num%10;
+			cubesum = cubesum+(lastDigit*lastDigit*lastDigit);   
+			num=num/10;	
+		}
+		if (cubesum==originalnum) {
+			 System.out.println(num +" is Armstrong number");
+		}
+		else{
+		     System.out.println(num +" is not Armstrong number");
 			
-		float total = subject1+subject2+subject3+subject4+subject5;
-		float Percentage = (total / 500) * 100;
-		 System.out.println("Total Marks =  " + total);
-		 System.out.println("Marks Percentage =  " + Percentage);
-		
-		if(Percentage >= 90)
-	    {
-			System.out.println("Grade A+");
 		}
-		else if(Percentage >= 75 && Percentage <= 89)
-	    {
-			System.out.println("Grade B");
-		}
-		else if(Percentage >= 60 && Percentage <= 74)
-	    {
-			System.out.println("Grade C");
-		}
-		else if(Percentage >= 50 && Percentage <= 59)
-	    {
-			System.out.println("Grade D");
-		}
-		else if(Percentage >= 33 && Percentage <= 49)
-	    {
-			System.out.println("Grade E");
-		}
-		else 
-	    {
-			System.out.println("Grade F");
-		} 
-		
+	   
 	}
 
 }
